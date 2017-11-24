c96212be6922a6e970505c7b1c05b377dc3836c1
dwzd <dwzd@users.noreply.github.com>
2017-11-24 12:17:01 -0500

    Add project files.

---
+++ GenericDelegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWork((a, b) => { return a * b; }, 1.2, 3.0);  //Type double here
+            DoWork((a, b) => { return a / b; }, 6, 2);      //Type int here
+            DoWork((a, b) => { return a + b; }, "lo", "ve!");//Type string here
+            Console.ReadKey();
+        }
+        static void DoWork<T>(Func<T, T, T> func1, T a, T b)
+        {
+            T result = func1(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
